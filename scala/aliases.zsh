f734a744b30f01a73f2be7797cb315b8c518c251
Nader Hajlaoui <hajlaoui.nader@gmail.com>
2020-11-07 16:17:43 +0100

    add scala skeleton

--- scala/aliases.zsh
+++ scala/aliases.zsh
@@ -1,3 +1,17 @@
 ##### SBT
 alias sbt='sbt -Dsbt.override.build.repos=true'
 alias newScalaProject='sbt new scala/scala-seed.g8'
+alias scalaAppSkeleton='mkdir app &                           
+touch app/docker-compose.yml &
+mkdir modules                 &       
+mkdir modules/core             &      
+mkdir modules/core/src          &     
+mkdir modules/core/src/main      &   
+mkdir modules/core/src/main/resources &
+mkdir modules/core/src/main/scala    &
+mkdir modules/tests/                 &
+mkdir modules/tests/src              &
+mkdir modules/tests/src/test         &
+mkdir modules/tests/src/test/scala   &
+mkdir modules/tests/src/main         &
+mkdir modules/tests/src/main/scala'
